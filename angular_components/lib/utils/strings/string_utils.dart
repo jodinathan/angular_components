5a3db7ea1c2685a3c906ed4a10631a4e56e74c10
GZGavinZhao <74938940+GZGavinZhao@users.noreply.github.com>
2021-10-07 23:32:02 -0400

    Migrate string_utils

--- angular_components/lib/utils/strings/string_utils.dart
+++ angular_components/lib/utils/strings/string_utils.dart
@@ -42,8 +42,9 @@ String underscore(String s) => _split(s).join('_').toLowerCase();
 ///  * "1337" => "1337"
 ///  * "foo3bar" => "Foo3bar"
 ///  * "3bar" => "3bar"
+/// WRITE UNIT TEST!
 String camelCase(String s) =>
-    s.replaceAllMapped(_wordBreak, (m) => m[2].toUpperCase());
+    s.replaceAllMapped(_wordBreak, (m) => m[2]!.toUpperCase());
 
 /// Returns the lower-camel-cased form of [s].
 String lowerCamelCase(String s) {
